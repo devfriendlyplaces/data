0c2f2abb9ec6e5ceba107221843483aaacc3dcfc
Pntbr <stephane.langlois@scopyleft.fr>
2017-05-15 20:44:20 +0200

    Keep only json files

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var should = chai.should();
-
-describe('Extract the location from the url', function() {
-  it('should return the location from an Url', function () {
-    var location = extractLocationFromUrl('cerclon.devfriendlyplaces.net');
-    location.should.equal('cerclon');
-  })
-})
